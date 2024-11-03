1ec8ac13125c316fdc16520d340e50dbdebc9c02
Tasnim2403 <2294sa4728rumc@gmail.com>
2024-11-03 21:42:09 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+a = int(input("Enter value of a: "))
+b = int(input("Enter value of b: "))
+c = int(input("Enter value of c: "))
+temp = a
+a = b
+b = c
+c = temp
+print("A =", a)
+print("B =", b)
+print("C =", c)
